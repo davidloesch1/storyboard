38c03667aed093587016f5e76a987d924f437b1f
David Loesch <davidloesch1@gmail.com>
2020-03-07 13:01:35 -0800

    testing my patience

---
+++ build/precache-manifest.7db097f0f8d66892019f0a9577e60ad3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7384d5d71717c64e2d42ca2e45b44ff5",
+    "url": "/storyboard/index.html"
+  },
+  {
+    "revision": "4fc4722fe714aad2ed36",
+    "url": "/storyboard/static/css/main.fe6effcc.chunk.css"
+  },
+  {
+    "revision": "20bb4179ba8a4d12118d",
+    "url": "/storyboard/static/js/2.334f08d4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/storyboard/static/js/2.334f08d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc4722fe714aad2ed36",
+    "url": "/storyboard/static/js/main.a3b5bf06.chunk.js"
+  },
+  {
+    "revision": "83596109d2dc8a6ae379",
+    "url": "/storyboard/static/js/runtime-main.16fa767d.js"
+  },
+  {
+    "revision": "20a5901d63b08f265ce817251773f3b5",
+    "url": "/storyboard/static/media/Bookworm vector 3.20a5901d.jpg"
+  },
+  {
+    "revision": "3501bbc77460b5d6fb7a118b79a26db3",
+    "url": "/storyboard/static/media/idea4.3501bbc7.jpg"
+  },
+  {
+    "revision": "e264008389acd9858d67fc29f8c87db8",
+    "url": "/storyboard/static/media/typewriter-2.e2640083.jpg"
+  }
+]);
